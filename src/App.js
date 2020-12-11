758f4197e68acbdaa8f54e03c8aa34b7166fe941
Brian Reisman <brianreisman@protonmail.com>
2020-12-11 11:57:02 -0500

    Character component rendering after mapping through state held in app.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Character from "./components/Character";
 import axios from "axios";
+import styled from "styled-components";
 
 const App = () => {
   const [state, setState] = useState([]);
-  console.log(state);
-
+  
   useEffect(() => {
     axios
       .get("https://swapi.dev/api/people")
@@ -16,19 +16,36 @@ const App = () => {
       .catch((err) => {
         console.error(err);
       });
-  }, []);
-
-
-const char = state.map((char, i) => {
-  console.log(char)
-  return <Character data={state[i]}/>
-})
+    }, []);
+    
+    const char = state.map((char, i) => {
+      return <Character data={state[i]} />;
+  });
+  
+  const StyledApp = styled.div`
+    /* background-color: red; */
+    display: flex;
+    flex-direction: column;
+    align-self: auto;
+    align-items: center;
+    *{
+      border: 1px solid blue;
+    }
+    p{
+      line-height: -1px;
+      letter-spacing: -1px;
+      margin: 8px;
+      border: 1px solid red;
+    }
+  `
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-      {char}
-    </div>
+    <StyledApp>
+      <div className="App">
+        <h1 className="Header">Characters</h1>
+        {char}
+      </div>
+    </StyledApp>
   );
 };
 
